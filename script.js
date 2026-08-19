e7097ae875e0e7bc88f6c3665f67fbc5ebaa77d8
niainakevinDev <niainakevin17@gmail.com>
2026-08-19 15:43:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -370,9 +370,9 @@ themeToggle.addEventListener('click', () => {
 
 // 👉 REMPLACE PAR TES PROPRES IDENTIFIANTS
 const EMAILJS_CONFIG = {
-    publicKey: 'YOUR_PUBLIC_KEY',
-    serviceID: 'YOUR_SERVICE_ID',
-    templateID: 'YOUR_TEMPLATE_ID'
+    publicKey: 'fVDbV4n8Vi2aKt-Rb',
+    serviceID: 'service_dkvqny6',
+    templateID: 'template_z6yofmo'
 };
 
 // Fonction d'envoi
